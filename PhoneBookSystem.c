d7506e6fd73fc7f1ecf4274d8c05cd8a81c39a8b
Burak <burakpala562@gmail.com>
2026-06-13 23:35:37 +0300

    Implemented alphabetical sorting for the linked list and modularized user input.

--- PhoneBookSystem.c
+++ PhoneBookSystem.c
@@ -1,45 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdbool.h>
 struct person{
 char name[100];
 char phoneNumber[20];
 struct person *next;
 };
 
-void addPerson(struct person **head,char *name,char *phoneNumber);
-
+void addPerson(struct person **head);
+bool getPersonInput(char *name , char *phoneNumber);
 
 int main(){
 struct person *head=NULL;
 }
 
+bool getPersonInput(char *name , char *phoneNumber){
 
-void addPerson(struct person **head,char *name,char *phoneNumber){
-
-struct person *newPerson=malloc(sizeof(struct person));
-if(newPerson==NULL) return;
 printf("Enter name: ");
-if(scanf("%99s", newPerson->name) != 1){
+if(scanf("%99s", name) != 1){
     fprintf(stderr, "Invalid name\n");
-    free(newPerson);
-    return;
+    return 0;
 }
 
 printf("Enter phone number: ");
-if(scanf("%19s", newPerson->phoneNumber) != 1){
+if(scanf("%19s", phoneNumber) != 1){
     fprintf(stderr, "Invalid number\n");
+    return 0;
+}
+return 1;
+}
+
+void addPerson(struct person **head){
+struct person *newPerson=malloc(sizeof(struct person));
+if(newPerson==NULL) return;
+newPerson->next=NULL;
+int control = getPersonInput(newPerson->name, newPerson->phoneNumber);
+
+    if(!control){
     free(newPerson);
     return;
 }
-newPerson->next=NULL;
+    struct person *previousPtr=NULL;
+    struct person *currentPtr=*head;
 
-struct person *previousPtr=NULL;
-struct person *currentPtr=*head;
+    while(currentPtr != NULL && strcmp(newPerson->name , currentPtr->name)>0){
 
-if(currentPtr==NULL){
-    *head=newPerson;
-}
+        previousPtr=currentPtr;
+        currentPtr=currentPtr->next;
+    }
+
+    if(previousPtr==NULL){
+        newPerson->next=*head;
+        *head=newPerson;
+    }
+
+    else{
+        previousPtr->next=newPerson;
+        newPerson->next=currentPtr;
+    }
 
 }
